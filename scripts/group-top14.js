50e083af2861cc7ca47cdb4c4952c98fbb72b707
Sono Yeung <sono@live.hk>
2022-09-13 22:45:58 +0800

    group top14

---
+++ scripts/group-top14.js
@@ -0,0 +1,52 @@
+const moment = require("moment");
+const fs = require("fs");
+
+const start = moment.unix(1662868800);
+const end = moment.unix(1663426800);
+
+const run = async () => {
+	const groups = {};
+
+	let t = moment.unix(start.unix());
+	while (t.isBefore(end)) {
+		groups[t.unix()] = [];
+
+		t = t.add("4", "hours");
+	}
+
+	const files = await fs.readdirSync("top14");
+
+	for (var file of files) {
+		for (var key of Object.keys(groups)) {
+			const fileTime = file.replace(".json", "");
+
+			if (
+				moment
+					.unix(fileTime)
+					.isBetween(
+						moment.unix(key),
+						moment.unix(key).add(4, "hours"),
+						undefined,
+						"[)"
+					)
+			) {
+				console.log(`${file} => ${key}.json`);
+				const fileContent = await fs.readFileSync(`top14/${file}`);
+				const data = JSON.parse(fileContent);
+				groups[key].push(data);
+			}
+		}
+	}
+
+	for (var key of Object.keys(groups)) {
+		console.log(`output ${key}.json`);
+		await fs.writeFileSync(
+			`top14-grouped/${key}.json`,
+			JSON.stringify(groups[key])
+		);
+	}
+
+	process.exit(0);
+};
+
+run();
